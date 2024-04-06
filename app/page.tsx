c574b902c6cb180dabd95d54a22f1baa1866d213
viraj458 <virajmalaka94@gmail.com>
2024-04-07 01:18:52 +0530

    Revert "feat: Camp section"
    This reverts commit 6f11c4acb9901e8f8e367fecc902af1040fb8ec0.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Camp from '../components/Camp'
-import Hero from '../components/Hero'
+import Camp from '../components/Camp';
+import Features from '../components/Features';
+import GetApp from '../components/GetApp';
+import Guide from '../components/Guide';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Camp />
     </>
-  )
+  );
 }
- 
