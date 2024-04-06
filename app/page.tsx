bcda64a57672d995e0d79df7ed7d01b6e2af46ab
viraj458 <virajmalaka94@gmail.com>
2024-04-06 12:03:26 +0530

    feat: Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Camp from '../components/Camp';
+import Features from '../components/Features';
+import GetApp from '../components/GetApp';
+import Guide from '../components/Guide';
+import Hero from '../components/Hero';
+
 export default function Home() {
-  return <h1 className="font-bold text-teal-500 text-2xl">Heyyy</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
